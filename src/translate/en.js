4d489df05388081613011f8b66f83e3bce01945e
Alan Araujo <alan.araujo@bennu.tv>
2019-07-30 02:03:35 -0300

    Implement translate multi files

---
+++ src/translate/en.js
@@ -0,0 +1,10 @@
+const en = {
+    translation: {
+        "not sure about size" : "NOT SURE ABOUT THE SIZE?",
+        "we help": "NO WORRIES, WE'LL HELP YOU!",
+        "find out": "FIND OUT WHAT SIZES FIT YOUR DOG BEST",
+        "select breed": "SELECTYOUR DOG`S BREED"
+    }
+}
+
+export default en
