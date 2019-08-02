fdcd0d3400bed68e3badae9c216ece9d601d2e75
Alan Araujo <alan.araujo@bennu.tv>
2019-08-02 00:49:49 -0300

    News images

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,11 +1,21 @@
 import logo from "./logo-horizontal-branco.webp";
 import btnPt from "./button-pt.webp";
 import btnEn from "./button-en.webp";
+import disclaimerPt from "./disclaimer-br.webp";
+import disclaimerEn from "./disclaimer-en.webp";
+import returnBtnEn from "./return-btn-en.webp";
+import returnBtnPt from "./return-btn.webp";
+import dropdownBallonPressed from "./dropdown-ballon-pressed.webp";
 
 const images = {
     logo,
     btnEn,
-    btnPt
+    btnPt,
+    disclaimerPt,
+    disclaimerEn,
+    returnBtnEn,
+    returnBtnPt,
+    dropdownBallonPressed
 }
 
 export default images;
