8d520cf8c71b8fe1788134a37846361346b6ec2e
Alan Araujo <alan.techsafe@hotmail.com>
2019-08-06 22:07:44 -0300

    Updates

---
+++ precache-manifest.60dd3a8c475b24d4b18f3c8466456d1f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0688675f53344a68e81cbfa625f60a",
+    "url": "/breeds-size/index.html"
+  },
+  {
+    "revision": "e5bccc162f416827ff4c",
+    "url": "/breeds-size/static/css/main.2022854d.chunk.css"
+  },
+  {
+    "revision": "5c602864e084cdbb1c51",
+    "url": "/breeds-size/static/js/2.1c0ddba5.chunk.js"
+  },
+  {
+    "revision": "e5bccc162f416827ff4c",
+    "url": "/breeds-size/static/js/main.a5371455.chunk.js"
+  },
+  {
+    "revision": "93b56f3a10a01f92d1ed",
+    "url": "/breeds-size/static/js/runtime~main.d388b1a2.js"
+  },
+  {
+    "revision": "4c08bea9168bb87deb463fc9a4972df9",
+    "url": "/breeds-size/static/media/button-en.4c08bea9.webp"
+  },
+  {
+    "revision": "f6f2c08f4a9d3e65a6ef8d6190d64d6d",
+    "url": "/breeds-size/static/media/button-pt.f6f2c08f.webp"
+  },
+  {
+    "revision": "2dc8f83701c55fdb35e13e335ca36688",
+    "url": "/breeds-size/static/media/disclaimer-br-breed.2dc8f837.webp"
+  },
+  {
+    "revision": "dfe3185b9013db563c8d4fd7318c9a8a",
+    "url": "/breeds-size/static/media/disclaimer-br.dfe3185b.webp"
+  },
+  {
+    "revision": "6435f57b44302931e62640fc5f45ecaf",
+    "url": "/breeds-size/static/media/disclaimer-en-breed.6435f57b.webp"
+  },
+  {
+    "revision": "3ff32e6fe8ef239187a2f68927aebbd1",
+    "url": "/breeds-size/static/media/disclaimer-en.3ff32e6f.webp"
+  },
+  {
+    "revision": "a3d0445ab63f650f440c6125c3ef3c5f",
+    "url": "/breeds-size/static/media/dropdown-balloon-en.a3d0445a.webp"
+  },
+  {
+    "revision": "c809cbe92a2f60201bf1236121250ea4",
+    "url": "/breeds-size/static/media/dropdown-balloon.c809cbe9.webp"
+  },
+  {
+    "revision": "82f8fe21b0026051725d814d19f95ff9",
+    "url": "/breeds-size/static/media/logo-horizontal-branco.82f8fe21.webp"
+  },
+  {
+    "revision": "dcfbcccc1160c7fe47c7b3cf5de305c9",
+    "url": "/breeds-size/static/media/return-btn-2-en.dcfbcccc.webp"
+  },
+  {
+    "revision": "c2ec6c6db0b77746abe5a30e615f592f",
+    "url": "/breeds-size/static/media/return-btn-2.c2ec6c6d.webp"
+  },
+  {
+    "revision": "3ed0da3f2e24fe3dbfef309e76db2105",
+    "url": "/breeds-size/static/media/return-btn-en.3ed0da3f.webp"
+  },
+  {
+    "revision": "348b4f16f66220dd46ebdb080088bb7f",
+    "url": "/breeds-size/static/media/return-btn.348b4f16.webp"
+  },
+  {
+    "revision": "947f7b34faffc80c2d4cc37e9f14cac3",
+    "url": "/breeds-size/static/media/table-background-en.947f7b34.webp"
+  },
+  {
+    "revision": "0230768e572e9098bd859b321718a8de",
+    "url": "/breeds-size/static/media/table-background.0230768e.webp"
+  },
+  {
+    "revision": "f456ad21273c3c97286f0d4de6a16219",
+    "url": "/breeds-size/static/media/td-bg-1-en.f456ad21.webp"
+  },
+  {
+    "revision": "70a7470eb39554190061a647dbec1534",
+    "url": "/breeds-size/static/media/td-bg-1.70a7470e.webp"
+  }
+]);
